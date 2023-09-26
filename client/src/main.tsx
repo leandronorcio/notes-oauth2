959802199f58fa7b0fe65f3e069389be5325b6fa
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-26 23:27:52 +0800

    Fixed SVG icons.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './globals.css';
 import App from './App';
 
-console.log(import.meta.env.VITE_GITHUB_AUTH);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
