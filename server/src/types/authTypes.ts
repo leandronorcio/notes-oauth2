68ef36e69f0d1995a218baf96a2131f6db8a0548
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-26 23:27:23 +0800

    Added `Account` and `User` types.

--- server/src/types/authTypes.ts
+++ server/src/types/authTypes.ts
@@ -1,7 +1,22 @@
+export type OAuthProviders = 'GITHUB' | 'GOOGLE' | 'FACEBOOK';
 export interface OAuthUser {
   id: string | number;
   name: string;
   // OAuth 2.0 providers do not always return an email
+  email?: string | null;
+  avatar?: string | null;
+}
+
+export interface Account {
+  id: number;
+  provider: OAuthProviders;
+  providerAccountId: string;
+  userId: number;
+}
+
+export interface User {
+  id: number;
+  name: string;
   email?: string | null;
   avatar?: string | null;
 }
