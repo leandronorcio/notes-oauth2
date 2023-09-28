d33d9024c407075701d60987790dde9eded476cc
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-28 13:52:30 +0800

    Implement `verifyJwt()` utility function.

--- server/src/controllers/auth/refresh.ts
+++ server/src/controllers/auth/refresh.ts
@@ -1,13 +1,14 @@
 import { RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
 import UserModel from '../../models/user';
+import { verifyJwt } from '../../utils/verifyJwt';
 
 export const refresh: RequestHandler = async (req, res) => {
   const token = req.cookies?.refreshToken;
   if (!token) return res.sendStatus(401);
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const decoded = verifyJwt(token);
     const user = await UserModel.findUnique(decoded.sub as unknown as number);
     if (!user) return res.sendStatus(401);
 
